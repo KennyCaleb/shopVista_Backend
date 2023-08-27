4bfffe114e58f40b94e729f33bf9e1c8480425fd
caleb <odedejikehinde08@gmail.com>
2023-08-27 09:08:37 -0700

    making correction

--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -23,19 +23,8 @@ router.post("/", asyncHandler(async(req, res)=>{
 
 router.get("/", asyncHandler(async(req, res)=>{
 
-    // const customers = await Customers.find()
-    res.status(200).json({msg:"All Customers", customers : {
-"id": 2,
-"title": "Mens Casual Premium Slim Fit T-Shirts ",
-"price": 22.3,
-"description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-"category": "men's clothing",
-"image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-"rating": {
-"rate": 4.1,
-"count": 259
-}
-}})
+    const customers = await Customers.find()
+    res.status(200).json({msg:"All Customers", customers})
 
 }))
 
