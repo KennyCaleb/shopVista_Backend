ecb68a579c83eb7f83002fc99f7034162234d65d
caleb <odedejikehinde08@gmail.com>
2023-09-01 10:16:19 -0700

    tried new DB  connection string

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
 // importing modules
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const error = require("./middleWare/errorMiddleWare");
-const customersRouter = require("./Routes/userRouter")
+const customersRouter = require("./Routes/userRouter");
 
 //connecting to database
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>{
-    console.log("Database connected successfully")
-})
-.catch((error)=>{
-    console.log(error)
-})
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 // middlewares
-app.use(express.json())
-app.use(error.errorMiddleWareHandler)
-app.use("/api/customers", customersRouter)
+app.use(express.json());
+app.use(error.errorMiddleWareHandler);
+app.use("/api/customers", customersRouter);
 
 // creating server
-const PORT = process.env.PORT || 5000
-app.listen(PORT, ()=>{
-    console.log(`Server running on PORT ${PORT}`)
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
+});
